7b6fff33cc90a6f4f643053e14382e34cf671181
seanwayland <seanwayland@gmail.com>
2020-02-20 20:46:14 -0500

    working script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,17 +1,21 @@
 import os
 import csv
+from collections import Counter
 
 
 #The total number of votes cast
 
 totalVotes = 0.0
 
-#A complete list of candidates who received votes
+#A complete list of candidates who recls
+# eived votes
 
-candidatesDictionary = {}
+candidatesArray = []
 
 currentCandidate = ""
 currentVotes = 0.0
+
+firstBool = True
 
 #The percentage of votes each candidate won
 
@@ -22,5 +26,41 @@ currentVotes = 0.0
 with open('election_data.csv', newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     for row in csvreader:
-        currentCandidate = row[0]
-        currentVotes = float(row[2])
+
+        if (firstBool == True):
+            firstBool = False
+        else:
+            currentCandidate = row[2]
+            candidatesArray.append(currentCandidate)
+#candidatesDict = Counter(candidatesArray)
+candidatesDict = {}
+for i in candidatesArray:
+  candidatesDict[i] = candidatesDict.get(i, 0) + 1
+  
+
+for j in candidatesDict:
+    totalVotes += candidatesDict[j]
+
+print(f'total votes recieved: {totalVotes} ')
+
+winnerVal = 0
+winner = ""
+print("candidates list: ")
+for key,val in candidatesDict.items():
+    print(key)
+
+print("")
+for key,val in candidatesDict.items(): 
+   
+
+    print(f' {key}  : recieved {val*100/totalVotes}  percent and total votes {val}')
+    if ((val/totalVotes) > winnerVal):
+        winnerVal = val/totalVotes
+       # print(winnerVal)
+        winner = key
+
+print(f'winner = {winner} ')
+
+
+
+            
